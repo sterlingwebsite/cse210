c1ac83f838644fcbbe9824863f17dba987fea10d
sterlingwebsite <ssteele1@byupathway.edu>
2026-01-10 14:55:28 -0700

    built classes and worked on instances and methods. W02 Project: Journal Program

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,47 @@
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string file)
+    {
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string file)
+    {
+        _entries.Clear();
+
+        string[] lines = File.ReadAllLines(file);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+
+            Entry newEntry = new Entry();
+            newEntry._date = parts[0];
+            newEntry._promptText = parts[1];
+            newEntry._entryText = parts[2];
+
+            _entries.Add(newEntry);
+        }
+    }
+}
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,20 @@
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>();
+
+    public PromptGenerator()
+    {
+        _prompts.Add("Who was the most interesting person you interacted with today?");
+        _prompts.Add("What was the best part of your day?");
+        _prompts.Add("How did you see the hand of the Lord in your life today?");
+        _prompts.Add("What was the strongest emotion you felt today?");
+        _prompts.Add("If you had one thing you could do over today, what would it be?");
+    }
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+}
