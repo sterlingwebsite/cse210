78aa542a387f5cbb2eae560882802cb412406180
sterlingwebsite <ssteele1@byupathway.edu>
2026-02-02 08:43:21 -0700

    W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>()
+        {
+            new Square("Blue", 5),
+            new Rectangle("Red", 4, 6),
+            new Circle("Green", 3)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
